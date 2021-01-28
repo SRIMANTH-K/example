08f6d19320410fd5b8fa50659f9019f6c4c7d52a
srimanth <srimanthk65@gmail.com>
2021-01-28 11:15:47 +0530

    this is done by srimanth

---
+++ src/test/java/demo/Example1.java
@@ -0,0 +1,17 @@
+package demo;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("this is test 1");
+		
+		System.out.println("This is test 2");
+		
+		System.out.println("This is test 3");
+		
+	}
+
+}
